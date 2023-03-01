25bd44c09e6d7c225741e563efc33d7f6ab55324
Kuosai <98367059+Kuosaii@users.noreply.github.com>
2023-03-01 15:50:59 -0500

    Initial commit

---
+++ src/main/java/net/risenteam/risencore/RisenCore.java
@@ -0,0 +1,12 @@
+package net.risenteam.risencore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RisenCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
