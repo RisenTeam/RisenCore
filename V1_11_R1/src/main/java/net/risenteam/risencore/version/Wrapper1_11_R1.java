99d659be045072f2b204e84a3a97354ed6b57a48
Kuosai <98367059+Kuosaii@users.noreply.github.com>
2023-03-02 13:39:27 -0500

    Adding and Updating Wrapper

---
+++ V1_11_R1/src/main/java/net/risenteam/risencore/version/Wrapper1_11_R1.java
@@ -0,0 +1,32 @@
+package net.risenteam.risencore.version;
+
+import net.minecraft.server.v1_11_R1.IChatBaseComponent;
+import net.minecraft.server.v1_11_R1.PacketPlayOutChat;
+import net.minecraft.server.v1_11_R1.PacketPlayOutTitle;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class Wrapper1_11_R1 implements RisenWrapper{
+
+    @Override
+    public void sendTitle(Player player, String title, String subtitle) {
+        PacketPlayOutTitle packetTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text \":\"" + title + "\"}"));
+        PacketPlayOutTitle packetSubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + subtitle + "\"}"));
+
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetTitle);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetSubtitle);
+    }
+
+    @Override
+    public void clearTitle(Player player) {
+        PacketPlayOutTitle packetClear = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.CLEAR, IChatBaseComponent.ChatSerializer.a(""));
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetClear);
+    }
+
+    @Override
+    public void sendActionBar(Player player, String message) {
+        PacketPlayOutChat packetActionBar = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}"), (byte) 2);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetActionBar);
+    }
+
+}
---
+++ V1_9_R2/src/main/java/net/risenteam/risencore/version/Wrapper1_9_R2.java
@@ -0,0 +1,32 @@
+package net.risenteam.risencore.version;
+
+import net.minecraft.server.v1_9_R2.IChatBaseComponent;
+import net.minecraft.server.v1_9_R2.PacketPlayOutChat;
+import net.minecraft.server.v1_9_R2.PacketPlayOutTitle;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class Wrapper1_9_R2 implements RisenWrapper{
+
+    @Override
+    public void sendTitle(Player player, String title, String subtitle) {
+        PacketPlayOutTitle packetTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text \":\"" + title + "\"}"));
+        PacketPlayOutTitle packetSubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + subtitle + "\"}"));
+
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetTitle);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetSubtitle);
+    }
+
+    @Override
+    public void clearTitle(Player player) {
+        PacketPlayOutTitle packetClear = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.CLEAR, IChatBaseComponent.ChatSerializer.a(""));
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetClear);
+    }
+
+    @Override
+    public void sendActionBar(Player player, String message) {
+        PacketPlayOutChat packetActionBar = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}"), (byte) 2);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetActionBar);
+    }
+
+}
